ad97041456f70bef2f0786e09e126884702e5136
Bolgov Roman <bolgovr@gmail.com>
2012-01-16 01:04:05 +0200

    add mongodb authentication&profile storage

--- app/authentication.js
+++ app/authentication.js
@@ -1,77 +1,96 @@
 var everyauth = require( 'everyauth' );
-var CONFIG = require( 'config' );
+var config = require( 'config' );
 var crypto = require( 'crypto' );
 var request = require( 'request' ); //for retrieving Facebook profile pic
+var User = require( './models.js' ).user;
 
 everyauth.facebook
-    .appId( CONFIG.auth.facebook.appId )
-    .appSecret( CONFIG.auth.facebook.appSecret )
+    .appId( config.auth.facebook.appId )
+    .appSecret( config.auth.facebook.appSecret )
     .handleAuthCallbackError( function (req, res) {
         console.log( 'callback error' );
     } )
     .findOrCreateUser( function (session, accessToken, accessTokExtra, fbUserMetadata) {
         var promise = this.Promise();
-        User.findOne( {oauth_token : accessToken}, {only : ['id', 'role', 'user_name', 'profile_pic_url']}, function (err, user) {
+        User.findByAuthProvider( 'facebook', accessToken, function (err, usr) {
             if (err) {
                 promise.fulfill( [err] );
             }
-
-            if (user === undefined) {
+            if (usr === undefined) {
                 var url = 'https://graph.facebook.com/' + fbUserMetadata.username + '?fields=picture&access_token=' + accessToken;
                 request( url, function (err, resp, body) {
                     var profile_pic = {'picture' : 'defaultpic.jpg'};
                     if (resp.statusCode === 200) {
                         profile_pic = JSON.parse( body );
                     }
-                    User.create( {
-                        'oauth_token' : accessToken,
-                        'user_name' : fbUserMetadata.name,
-                        'role' : CONFIG.App.User.default_role,
-                        'email' : fbUserMetadata.email,
-                        'profile_pic_url' : profile_pic.picture
-
-                    }, function (err, newUser) {
+                    var user = new User();
+                    user.auth.facebook.accessToken = accessToken;
+                    user.auth.facebook.userId = fbUserMetadata.id;
+                    user.role = config.App.User.default_role;
+                    user.email = fbUserMetadata.email;
+                    user.profile_pic_url = profile_pic.picture;
+                    user.name = fbUserMetadata.name;
+                    user.profile_url = fbUserMetadata.link;
+                    user.auth.facebook.metadata = fbUserMetadata;
+                    user.save( function (err, done) {
                         if (err) {
                             promise.fulfill( [err] );
-                        } else {
-                            var newUsr = newUser.rows[0];
-                            var usr = {
-                                'id' : newUsr.id,
-                                'user_name' : newUsr.user_name,
-                                'role' : newUsr.role,
-                                'profile_pic_url' : newUsr.profile_pic_url
-                            };
-                            session.info = usr;
-                            promise.fulfill( usr );
+                            return undefined;
                         }
-                    } )
+                        promise.fulfill( data );
 
+                    } );
                 } );
-
             } else {
-                session.info = user;
-                promise.fulfill( user );
+                promise.fulfill( usr );
             }
         } );
         return promise;
 
     } )
-    .redirectPath( CONFIG.auth.facebook.redirectPath )
-    .entryPath( CONFIG.auth.facebook.entryPath )
-    .callbackPath( CONFIG.auth.facebook.callbackPath )
-    .scope( CONFIG.auth.facebook.scope );
+    .redirectPath( config.auth.facebook.redirectPath )
+    .entryPath( config.auth.facebook.entryPath )
+    .callbackPath( config.auth.facebook.callbackPath )
+    .scope( config.auth.facebook.scope );
 
 everyauth.twitter
-    .consumerKey( CONFIG.auth.twitter.consumerKey )
-    .consumerSecret( CONFIG.auth.twitter.consumerSecret )
+    .consumerKey( config.auth.twitter.consumerKey )
+    .consumerSecret( config.auth.twitter.consumerSecret )
     .findOrCreateUser( function (session, accessToken, accessTokenSecret, twitterUserMetadata) {
-        console.log( arguments );
-        process.exit( 1 );
+        var promise = this.Promise();
+        User.findByAuthProvider( 'twitter', accessToken, function (err, usr) {
+            if (err) {
+                promise.fulfill( [err] );
+            }
+            if (usr === undefined) {
+                var user = new User();
+                user.auth.twitter.token = accessToken;
+                user.auth.twitter.userId = twitterUserMetadata.id;
+                user.auth.twitter.metadata = twitterUserMetadata;
+                user.role = config.App.User.default_role;
+                user.profile_pic_url = twitterUserMetadata.profile_image_url;
+                user.name = twitterUserMetadata.name;
+                user.profile_url = "https://twitter.com/#!/"+twitterUserMetadata.screen_name;
+
+                user.save( function (err, done) {
+                    if (err) {
+                        promise.fulfill( [err] );
+                        return undefined;
+                    }
+                    promise.fulfill( data );
+
+                } );
+
+            } else {
+                promise.fulfill( usr );
+            }
+        } );
+        return promise;
 
     } )
-    .redirectPath( CONFIG.auth.twitter.redirectPath )
-    .entryPath( CONFIG.auth.twitter.entryPath )
-    .callbackPath( CONFIG.auth.twitter.callbackPath );
+    .redirectPath( config.auth.twitter.redirectPath )
+    .entryPath( config.auth.twitter.entryPath )
+    .callbackPath( config.auth.twitter.callbackPath );
 
 function encryptPassword (psw, salt) {
     var hash = crypto.createHash( 'md5' );
@@ -83,24 +102,24 @@ function encryptPassword (psw, salt) {
 /*
  everyauth.password
  .loginWith('email')
- .getLoginPath(CONFIG.Views.login_path)
- .postLoginPath(CONFIG.Views.login_path)
- .loginView(CONFIG.Views.login)
+ .getLoginPath(config.Views.login_path)
+ .postLoginPath(config.Views.login_path)
+ .loginView(config.Views.login)
  .authenticate(function (login, password, request) {
  var errors = [];
- if (!login) errors.push(CONFIG.Phrases.empty_email);
- if (!password) errors.push(CONFIG.Phrases.empty_password);
+ if (!login) errors.push(config.Phrases.empty_email);
+ if (!password) errors.push(config.Phrases.empty_password);
  if (errors.length) return errors;
 
 
  var promise = this.Promise();
- var secretPsw = encryptPassword(password, CONFIG.Session.salt);
+ var secretPsw = encryptPassword(password, config.Session.salt);
  User.findOne({login:login, password:secretPsw}, {only:['id', 'role', 'user_name', 'profile_pic_url']}, function (err, user) {
  if (err) {
  promise.fulfill([err]);
  } else {
  if (user === undefined) {
- promise.fulfill([CONFIG.Phrases.login_and_psw_pair_not_found]);
+ promise.fulfill([config.Phrases.login_and_psw_pair_not_found]);
  } else {
  promise.fulfill(user);
  }
@@ -108,17 +127,17 @@ function encryptPassword (psw, salt) {
  });
  return promise;
  })
- .loginSuccessRedirect(CONFIG.Views.login_success_path)
- .getRegisterPath(CONFIG.Views.register_path)// Uri path to the registration page
- .postRegisterPath(CONFIG.Views.register_path)// The Uri path that your registration form POSTs to
- .registerView(CONFIG.Views.register, {layout:true})
+ .loginSuccessRedirect(config.Views.login_success_path)
+ .getRegisterPath(config.Views.register_path)// Uri path to the registration page
+ .postRegisterPath(config.Views.register_path)// The Uri path that your registration form POSTs to
+ .registerView(config.Views.register, {layout:true})
  .validateRegistration(function (newUserAttributes) {
  var errors = [];
  if (!newUserAttributes.email) {
- errors.push(CONFIG.Phrases.empty_email);
+ errors.push(config.Phrases.empty_email);
  }
  if (!newUserAttributes.password) {
- errors.push(CONFIG.Phrases.empty_password);
+ errors.push(config.Phrases.empty_password);
  }
  if (errors.length > 0) {
  return errors;
@@ -129,7 +148,7 @@ function encryptPassword (psw, salt) {
  promise.fulfill([err]);
  } else {
  if (email !== undefined) {
- promise.fulfill([CONFIG.Phrases.email_already_taken]);
+ promise.fulfill([config.Phrases.email_already_taken]);
  } else {
  promise.fulfill(newUserAttributes);
  }
@@ -140,7 +159,7 @@ function encryptPassword (psw, salt) {
 
  })
  .registerUser(function (newUserAttributes) {
- var secretPsw = encryptPassword(newUserAttributes.password, CONFIG.Session.salt);
+ var secretPsw = encryptPassword(newUserAttributes.password, config.Session.salt);
  var promise = this.Promise();
  User.create({'login':newUserAttributes.email, 'email':newUserAttributes.email, 'password':secretPsw}, {only:['id', 'role', 'user_name']}, function (err, User) {
  if (err) {
@@ -156,6 +175,6 @@ function encryptPassword (psw, salt) {
  });
  return promise;
  })
- .registerSuccessRedirect(CONFIG.Views.login_success_path); // Where to redirect to after a successful registration
+ .registerSuccessRedirect(config.Views.login_success_path); // Where to redirect to after a successful registration
  */
 module.exports = everyauth;
--- app/models/user.js
+++ app/models/user.js
@@ -3,16 +3,41 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var User = new Schema( {
-    'auth' : {
-        'twitter' : {
-
+    'name':String,
+    'profile_pic_url':String,
+    'level' : {'type' : Number, 'min' : 0, 'max' : 3},
+    'email':String, //TODO: replace with mongoose-types email type(adds free validation)
+    'profile_url':String,
+    'auth':{
+        'twitter':{
+            'token':String,
+            'userId':Number,
+            'metadata':{}
         },
-        'facebook' : {
-
+        'facebook':{
+            'accessToken':String,
+            'userId':Number,
+            'metadata':{}
+        },
+        'password':{
+            'login':String,
+            'psw':String
         }
     }
-
 } );
+
+User.methods.findByAuthProvider = function(type,token,cb){
+    switch(type){
+        case 'twitter':this.findOne({'auth.twitter.token':token}).run(cb);
+            break;
+        case 'facebook':this.findOne({'auth.facebook.accessToken':token}).run(cb);
+            break;
+        case 'password':this.findOne({'auth.password.login':token.login,'auth.password.psw':token.psw}).run(cb);
+            break;
+        default:cb(new Error('unknown auth type'));
+            break;
+    }
+};
 
 module.exports = User;
 
