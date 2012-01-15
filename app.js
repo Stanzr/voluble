5ecaeaa567420f31260c3e93311bdb45ed2693fc
Bolgov Roman <bolgovr@gmail.com>
2012-01-15 02:19:03 +0200

    split configuration and launch an application into different files
    It will be useful when we start it with "cluster"

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var CONFIG = require('config');
+var res = require('express-resource');
+var auth = require('./app/authentication.js');
+var express_configuration = require('./app/express_configuration.js');
+var app = express.createServer();
+var io = require('socket.io').listen(app);
+var jade = require('jade');
+var express_routes = require('./app/express_routes.js');
+var socket_routes = require('./app/socket_routes.js');
+
+/**
+ * app settings
+ */
+express_configuration.configure(app);
+
+
+
+
+/**
+ * Routes
+ */
+
+express_routes.configure(app);
+socket_routes.configure(io);
+
+
+module.exports = app;
