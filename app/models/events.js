74f102051e9ead1bb39f2605b5fbdb67c8c6bada
Bolgov Roman <bolgovr@gmail.com>
2012-01-19 01:58:05 +0200

    minor changes in event model

--- app/models/events.js
+++ app/models/events.js
@@ -3,13 +3,17 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
 var Event = new Schema( {
-    'name' : String,
-    'info' : String,
-    'summary' : String,
-    'status':String,
-    'event_date':Date,
+    'name' : {'type':String,'required':true},
+    'info' : {'type' : String, 'required' : true},
+    'summary' : {'type' : String, 'required' : true},
+    'status':{'type':String,'enum':['planned','started','finished']},
+    'start_date':{'type':Date,'default':Date.now},
     'attendees' : [ObjectId]
 } );
+
+Event.statics.findCurrentEvents = function(cb){
+   this.find({'start_date':{'$lt':Date.now()},'status':{'$ne':'finished'}},cb);
+};
 
 module.exports = Event;
 
