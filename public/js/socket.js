3174254ea18426027dc1bc69ccaaeaf18a90c688
Bolgov Roman <bolgovr@gmail.com>
2012-01-19 02:58:52 +0200

    minor improvements
    every page by default will attempt to connect into chat by current
    document URL

--- public/js/socket.js
+++ public/js/socket.js
@@ -2,20 +2,14 @@ var socket = io.connect( document.location.origin );
 socket.on( 'systemMsg', function (data) {
    console.log('systemMsg!',data);
 } );
-var chat = 'testSMOChat';
+var chat = document.location.pathname;
 socket.emit('reqForChatJoin', chat);
 socket.on('resForChatJoin',function(isAllowed){
     if(!isAllowed){
         alert('cant join ' + chat);
-    }else{
-        alert('joined to ' + chat)
     }
 });
 
 socket.on('chatMsg',function(data){
    console.log('chat msg!',data);
 });
-
-setInterval(function(){
-    socket.emit('chatMsg',{'msg':'hell yeah'});
-},5000);
