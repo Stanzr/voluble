76ce3bcd147ec3f2a99ae714600f82bc4434f9e4
Bolgov Roman <bolgovr@gmail.com>
2012-01-19 02:00:08 +0200

    add functionality for current chat's list

--- app/resources/chat/user_chat.js
+++ app/resources/chat/user_chat.js
@@ -1,20 +1,25 @@
 var options = {};
 
-function setOptions (opts) {
-    options = opts;
-    return api;
 
-}
-exports.configure = setOptions;
 var api = {};
 api.index = function (req, res) {
-    res.end( 'hi from chat' );
+    options.model.event.findCurrentEvents(function(err,evts){
+        res.render('dashboard',{
+            'title' : 'Available chats',
+            'data' : {
+                'events' : evts,
+                'past_events' : undefined
+            }});
+    })
 };
-api.new = function (req, res) {
+api['new'] = function (req, res) {
 };
 api.create = function (req, res) {
 };
 api.show = function (req, res) {
+    res.render('CUloggedin',{
+        'title':"Welcome to "+req.params.chat
+    });
 };
 api.edit = function (req, res) {
 
@@ -23,3 +28,10 @@ api.update = function (req, res) {
 };
 api.destroy = function (req, res) {
 };
+
+function setOptions (opts) {
+    options = opts;
+    return api;
+
+}
+exports.configure = setOptions;
