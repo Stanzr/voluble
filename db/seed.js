3f73b1e63e6bff4de1596d05f6ce38929fe1212b
Bolgov Roman <bolgovr@gmail.com>
2012-01-19 02:59:18 +0200

    add user and chatmsg to seed

--- db/seed.js
+++ db/seed.js
@@ -36,9 +36,36 @@ function PastEvent (callback) {
     } )).save( callback );
 }
 
+var users = [];
+
+function User(callback){
+    (new models.user({
+        'name' : Faker.Name.firstName(),
+        'profile_pic_url' : '/images/user_001.png',
+        'level' : Faker.Helpers.randomNumber(3),
+        'email' : Faker.Internet.email(), //TODO: replace with mongoose-types email type(adds free validation)
+        'profile_url' : Faker.Internet.domainName()
+    })).save(function(err,usr){
+        users.push(usr);
+        callback();
+    });
+}
+function ChatMsg(callback){
+    var user = Faker.Helpers.randomize(users);
+    (new models.chatMsg({
+        'user' : {
+            'slug':user.name,
+            'name':user.name
+        },
+        'chatId' : 'Jovanny',
+        'message' : Faker.Lorem.sentences(3),
+        'type' : 'user'
+    })).save(callback);
+}
+
 var seed = new Seeder();
 
 exports.run = function (cb) {
-    seed.create().next( 15, FutureEvent ).next(20,PastEvent).next(10, CurrentEvent).run( cb );
+    seed.create().next( 15, FutureEvent ).next(20,PastEvent).next(10, CurrentEvent).next(50,User).next(100,ChatMsg).run( cb );
 };
 
