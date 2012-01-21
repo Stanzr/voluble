a33d9e2a4a362d944a23262c1b087d22d9063fa9
Bolgov Roman <bolgovr@gmail.com>
2012-01-21 03:18:11 +0200

    initial commit of chat's module

--- public/js/socket.js
+++ public/js/socket.js
@@ -9,6 +9,26 @@ socket.on('resForChatJoin',function(isAllowed){
         alert('cant join ' + chat);
     }
 });
+/*
+$('.send').live('click',function(){
+    var msg = $('#chatMsg').val();
+    if(msg){
+        socket.emit('chatMsg',msg);
+    }
+});
+*/
+$(window).ready(function(){
+    $('#chatForm').submit(function(evt){
+        evt.stopPropagation();
+        var msg = $('#chatMsg').val();
+        if (msg){
+            socket.emit('chatMsg', msg);
+            $('#chatMsg').val('');
+        }
+        return false;
+    });
+});
+
 
 socket.on('chatMsg',function(data){
    console.log('chat msg!',data);
