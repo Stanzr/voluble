c1dafa0cf9541cd722bd07c801bffb95f60a05f5
Bolgov Roman <bolgovr@gmail.com>
2012-01-28 20:28:34 +0200

    chatMsg model with tests

--- test/chatMsg-module-test.js
+++ test/chatMsg-module-test.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
 var chatMessage = require('../app/modules/chatMsg.js');
-var chatMessageModel = require('../app/models.js');
+var chatMessageModel = require('../app/models.js').chatMsg;
 
 describe('chatMsg',function(){
   var chatMsg,chatId;
@@ -11,14 +11,43 @@ describe('chatMsg',function(){
   });
   it('should return error if message isnt correct',function(done){
     var msg = {'msg':'hi'};
-    
+
     chatMsg.processMessage(msg,chatId,function(err){
       assert.equal(err instanceof Error,true);
       assert.equal(err.message,'Cant process message');
       done();
     });
+
+  });
+  
+  it('should create msg model',function(done){
+    var msg = {'msg':'test1','sender':{'user':'test'}};
+    var testMsg = chatMsg.create(msg);
+    assert.ok(testMsg);
+    assert.ok(testMsg._id);
+    assert.ok(testMsg instanceof chatMessageModel);
+    done();
   
   });
 
+  it('should add message to db',function(done){
+    var msg = {
+      'msg':'test1',
+      'sender':{
+        'profile_url':'google.com',
+        'name':'test user',
+        'profile_pic_url':'default.jpg'
+      }
+    };
+    chatMsg.processMessage(msg,chatId,function(err,message){
+      assert.equal(err,null);
+      chatMessageModel.findById(message._id,function(err,msg){
+        assert.ok(msg);
+        done();
+      });
 
+    });
+
+
+  });
 });
--- test/chatmessage-test.js
+++ test/chatmessage-test.js
@@ -27,6 +27,7 @@ describe('chatMessage',function(){
   afterEach(function(done){
     ChatMsg.collection.remove({},done); 
   });
+
   it('in room test should be exist 2 messages from Tony and Jake in correct order',function(done){
     ChatMsg.findByChat('test',function(err,messages){
       assert.equal(messages.length,2);
@@ -34,7 +35,50 @@ describe('chatMessage',function(){
       assert.equal(messages[1].user.name,'Jake');
       done();
     });
+    describe("sorting",function(){
+      beforeEach(function(done){
 
+        async.parallel([
+                       function(cb){
+          cb(null,null);
+        },
+        function(cb){
+          (new ChatMsg({'user':{'name':'Tony'},'chatId':'sorting','message':'message1','type':'user'})).save(cb);
+        },
+        function(cb){
+          (new ChatMsg({'user':{'name':'Jake'},'chatId':'sorting','message':'message2','type':'user'})).save(cb);
+
+        },
+
+        function(cb){
+          (new ChatMsg({'user':{'name':'Jake'},'chatId':'sorting','message':'message3','type':'user'})).save(cb);
+
+        },
+
+        function(cb){
+          (new ChatMsg({'user':{'name':'Jake'},'chatId':'sorting','message':'message4','type':'user'})).save(cb);
+
+        },
+
+        function(cb){
+          (new ChatMsg({'user':{'name':'Jake'},'chatId':'sorting','message':'message5','type':'user'})).save(cb);
+
+        }],done);
+      });
+      it('should correct order when pulled from DB',function(done){
+        ChatMsg.findByChat('sorting',function(err,messages){
+          assert.equal(err,null);
+          assert.equal(messages[0].message,'message1');
+          assert.equal(messages[1].message,'message2');
+          assert.equal(messages[2].message,'message3');
+          assert.equal(messages[3].message,'message4');
+          assert.equal(messages[4].message,'message5');
+          done(); 
+        });
+
+      });
+
+    });
   });
   describe('#like functionality',function(){
     var message;
