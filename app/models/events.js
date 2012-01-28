d00e84148ddfad07d999f0455d0368acdf55eef0
Bolgov Roman <bolgovr@gmail.com>
2012-01-28 17:00:48 +0200

    reformat events model

--- app/models/events.js
+++ app/models/events.js
@@ -7,17 +7,19 @@ var Event = new Schema( {
     'name' : {'type':String,'required':true},
     'info' : {'type' : String, 'required' : true},
     'summary' : {'type' : String, 'required' : true},
-    'status':{'type':String,'enum':['planned','started','finished']},
     'start_date':{'type':Date,'default':Date.now},
+    'end_date':{'type':Date,'default':Date.now},
     'attendees' : [ObjectId],
     'questions':[{}]
 } );
 
 Event.statics.findCurrentEvents = function(cb){
-    this.find({'start_date' : {'$gte' : new Date()}}).limit(10).run(cb);
+  var currentDate = new Date();
+  this.find({'start_date':{'$lte':currentDate},'end_date':{'$gte':currentDate}}).run(cb);
 };
 Event.statics.findPastEvents = function(cb){
-   this.find({'start_date':{'$lt':new Date()}}).limit(10).run(cb);
+  var currentDate = new Date(); 
+  this.find({'end_date':{'$lte':currentDate}}).limit(10).run(cb);
 };
 Event.statics.findEvent = function(name,cb){
     this.findOne({'name':name},cb);
