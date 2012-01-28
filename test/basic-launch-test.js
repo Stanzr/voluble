09b7145bce7e211ad0df4e900d225475fd52dae6
Bolgov Roman <bolgovr@gmail.com>
2012-01-28 17:00:21 +0200

    introduce basic tests with zombie
    close #15

--- test/basic-launch-test.js
+++ test/basic-launch-test.js
@@ -1,28 +1,63 @@
 var config = require( 'config' );
-
+var Browser = require('zombie');
+var assert = require('assert');
 describe( "Application", function () {
-
-    describe( "http", function () {
-        it( "should be listening by http", function (done) {
-            var app = require( '../app' ).express;
-            app.listen( config.server.port, function () {
-                app.close();
-                done();
-            } );
-        } );
+  describe( "#http", function () {
+    it( "should be listening by http", function (done) {
+      var app = require( '../app' ).express;
+      app.listen( config.server.port, function () {
+        app.close();
+        done();
+      } );
     } );
-    describe( "socket.io", function () {
-        it( "should listen to socket connection", function (done) {
-            var app = require( '../app' ).io;
-            var express = require('../app').express;
-            var l = console.log;
-            console.info = function(data){
-                l('stubbed '+data );
-            };
-            express.listen( config.server.port, function () {
-                done();
-            } );
-        } )
+  } );
+  describe( "#socket.io", function () {
+    it( "should listen to socket connection", function (done) {
+      var app = require( '../app' ).io;
+      var express = require('../app').express;
+      var l = console.log;
+      console.info = function(data){
+        l('stubbed '+data );
+      };
+      express.listen( config.server.port, function () {
+        done();
+      } );
     } )
+  } );
+  describe('#running',function(){
+    var browser,app;
+    beforeEach(function(done){
+      app = require( '../app' ).express;
+      app.listen( config.server.port, function () {
+        browser = new Browser();
+        browser.site = 'http://localhost:8000';
+          done();
+      });
+    });
+    it('should answer to / request with 200x code',function(done){
+      browser.visit('/',function(e,browser){
+        assert.ok(browser.success);
+        done();
+      }); 
+    });
+    describe('#main page',function(){
+      var browser;
+      beforeEach(function(done){
+        var tmp = new Browser();
+        tmp.site = 'http://localhost:8000';
+          tmp.visit('/',function(e,pagehandler){
+          browser = pagehandler;
+          done();
+        });
+      });
 
+      it('should have correct title',function(done){
+        assert.equal(browser.text('title'),'Chat Management - Dashboard');
+        done(); 
+      });
+
+      it('should have correct events on main page');
+    });
+
+  }); 
 } );
