ffb8b11d4ab23a538aca11a7700641f807b44c8e
Bolgov Roman <bolgovr@gmail.com>
2012-02-04 22:53:11 +0200

    rename chat list model events to less confusing

--- app/resources/socket/chatlist.js
+++ app/resources/socket/chatlist.js
@@ -2,17 +2,17 @@ var chatList = require('../../models.js').chatList;
 var _ = require('underscore')._;
 exports.configure = function(socket,io){
   var global = io;
-  socket.on('chats:read',function(data,callback){
+  socket.on('chatLists:read',function(data,callback){
       chatList.findCurrentEvents(function(err,results){
         callback(null,results);
       });
   });
-  socket.on('pastChats:read',function(data,callback){
+  socket.on('pastChatLists:read',function(data,callback){
     chatList.findPastEvents(function(err,results){
         callback(null,results);
     });
   });
-  socket.on('chats:create',function(data,callback){
+  socket.on('chatLists:create',function(data,callback){
     //TODO: replace this lines when template for creating project will arrive
     data.end_date = data.end_date || (new Date((new Date()).setDate((new Date()).getDate()+5)));
      
@@ -21,8 +21,8 @@ exports.configure = function(socket,io){
         console.log(err);
         return;
       }
-      socket.emit('chats:create', freshChat);
-      socket.broadcast.emit('chats:create', freshChat);
+      socket.emit('chatLists:create', freshChat);
+      socket.broadcast.emit('chatLists:create', freshChat);
     });
   });
 };
