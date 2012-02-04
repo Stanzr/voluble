d086f52e74d61a20511a8b4985d014f3fc3e9dde
Bolgov Roman <bolgovr@gmail.com>
2012-02-04 22:57:29 +0200

    rename event listener

--- app/resources/socket/chat.js
+++ app/resources/socket/chat.js
@@ -2,7 +2,7 @@ var ChatMsg = require('../../models.js').chatMsg;
 var _ = require('underscore')._;
 exports.configure = function(socket,io){
   var global = io;
-  socket.on('chatMsg:read',function(data,callback){
+  socket.on('chatMsgs:read',function(data,callback){
       callback(null,false);
   });
 };
