06022afe837ffc52797be06584adc74a9c580b41
Bolgov Roman <bolgovr@gmail.com>
2012-02-05 16:50:30 +0200

    add chatmsg creation and get later functions

--- app/resources/socket/chat.js
+++ app/resources/socket/chat.js
@@ -3,7 +3,17 @@ var _ = require('underscore')._;
 exports.configure = function(socket,io){
   var global = io;
   socket.on('chatMsgs:read',function(data,callback){
-      callback(null,false);
+    ChatMsg.findByChat(data[0].chatId, function(err,data){
+      callback(null,data);
+    });
+  });
+  socket.on('chatMsgs:create',function(data,callback){
+    if(!socket.handshake||!socket.handshake.user|| socket.handshake.user.user.name==='Guest'){
+      return false;
+    }
+    ChatMsg.processMessage(data,socket.handshake.user,function(err,msg){
+      callback(err,msg); 
+    });
   });
 };
 
