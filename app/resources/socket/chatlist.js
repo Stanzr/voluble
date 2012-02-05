a81de90869820c065db6cc1cb255fe4af2d962d7
Bolgov Roman <bolgovr@gmail.com>
2012-02-05 02:41:27 +0200

    add questions to default event creation

--- app/resources/socket/chatlist.js
+++ app/resources/socket/chatlist.js
@@ -15,7 +15,7 @@ exports.configure = function(socket,io){
   socket.on('chatLists:create',function(data,callback){
     //TODO: replace this lines when template for creating project will arrive
     data.end_date = data.end_date || (new Date((new Date()).setDate((new Date()).getDate()+5)));
-     
+    data.questions = ['q1','q2','q3'];    
     (new chatList(data)).save(function(err,freshChat){
       if(err){
         console.log(err);
