f6bd729db2256b867291d03e6c3d5d90da79d4f0
Bolgov Roman <bolgovr@gmail.com>
2012-02-05 16:50:05 +0200

    refactor chatmsg model

--- app/models.js
+++ app/models.js
@@ -6,5 +6,5 @@ module.exports = {
     'session' : new (require( 'connect' ).session.MemoryStore)(),
     'user' : mongoose.model( 'users', require( './models/user.js' ) ),
     'chatList' : mongoose.model( 'events', require( './models/events.js' ) ),
-    'chatMsg' : mongoose.model( 'chat_messages', require( './models/chatmessage.js' ) )
+    'chatMsg' : (require( './models/chatmessage.js' )).configure(mongoose) 
 };
--- app/models/chatmessage.js
+++ app/models/chatmessage.js
@@ -1,33 +1,60 @@
-var mongoose = require( 'mongoose' );
-var Schema = mongoose.Schema;
-var ObjectId = Schema.ObjectId;
+exports.configure=function(mongoose){
+  var Schema = mongoose.Schema;
+  var ObjectId = Schema.ObjectId;
 
-var ChatMsg = new Schema( {
-    'user' : {},
-    'created_at' : {'type':Date,'default':Date.now},
-    'chatId':String,
-    'message' : String,
-    'likes':[],
-    'replies':[],
-    'likeCount':{'type':Number,'default':0},
-    'type' : {'type':String, 'enum':['system','user','attention']}
-} );
-ChatMsg.statics.findByChat= function(chatId,cb){
-    this.find({'chatId':chatId}).sort('created_at',1).limit(10).run(cb);
-};
+  var ChatMsgSender = new Schema({
+    'name': String,
+    'uid': ObjectId,
+    'profile_pic_url':String
+  });
 
-
-ChatMsg.statics.like = function(msg,user,cb){
-  this.findById(msg.id,function(err,result){
-        result.likes.push(user);
-        result.likeCount+=1;
-        result.save(cb);
+  var ChatMsg = new Schema({
+    'user' : {
+      'name': String,
+      'uid': ObjectId,
+      'profile_pic_url':String
+    },
+    'created_at' : {'type': Date, 'default': Date.now},
+    'chatId': {'type': String, required: true},
+    'message' : {'type': String, required: true},
+    'likes': [],
+    'replies': [],
+    'likeCount': {'type': Number, 'default': 0},
+    'type': {'type': String, 'enum': ['system','user','attention']}
+  } );
+  ChatMsg.statics.findByChat= function(chatId,cb){
+    this.find({'chatId':chatId}).desc('created_at').limit(10).run(function(err,results){
+      cb(err,results.reverse());      
     });
-};
+  };
 
-ChatMsg.statics.findMostLiked = function(chat,cb){
+
+  ChatMsg.statics.like = function(msg,user,cb){
+    this.findById(msg.id,function(err,result){
+      result.likes.push(user);
+      result.likeCount+=1;
+      result.save(cb);
+    });
+  };
+
+  ChatMsg.statics.findMostLiked = function(chat,cb){
     this.find({'chatId':chat,'likeCount':{'$gt':0}}).sort('likeCount',-1).limit(4).run(cb);
-};
+  };
 
-module.exports = ChatMsg;
+  ChatMsg.statics.processMessage = function(msgData,user,cb){
+    if(msgData.user._id == user.user._id){
+      var msg = new this();
+      msg.user.name = user.user.name;
+      msg.user.uid = user.user._id;
+      msg.user.profile_pic_url = user.user.profile_pic_url;
+      msg.message = msgData.message;
+      msg.chatId = msgData.chatId;
+      msg.type = 'user';
+      msg.save(cb);
+    }else{
+      cb(new Error('unauthorized'));
+    }
+  };
+  return mongoose.model('chat_messages',ChatMsg);
+};
 
