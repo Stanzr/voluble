c2e46c2185cbec35919d97276e41fea31f7a602e
Bolgov Roman <bolgovr@gmail.com>
2012-02-08 01:15:14 +0200

    introduce chat participant view

---
+++ public/js/BackboneApp/views/chatParticipants.js
@@ -0,0 +1,27 @@
+(function (win) {
+  var Voluble = win.Voluble = win.Voluble || {};
+  var Backbone = win.Backbone, templates = Voluble.Templater;
+  templates.preCache('chatParticipant');  
+
+  Voluble.ChatParticipantsSingleView = Backbone.View.extend({
+    'tagName': "li",
+    'initialize': function () {
+      this.model.bind("change", this.render, this);
+      this.model.bind("destroy", this.close, this);
+    },
+    'render': function () {
+      var self = this;
+      var model = this.model.toJSON();
+      templates.render('chatParticipant', function (template) {
+        $(self.el).html(template({
+          'user': model
+        }));
+      });
+      return this;
+    },
+    'close': function () {
+      $(this.el).unbind();
+      $(this.el).remove();
+    }
+  });
+})(this);
