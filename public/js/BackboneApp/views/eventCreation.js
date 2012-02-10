ceb75d172440ae08f7a9c2d2f1c5c83bfd84f9d5
Bolgov Roman <bolgovr@gmail.com>
2012-02-10 02:40:06 +0200

    introduce templates for event creation

---
+++ public/js/BackboneApp/views/eventCreation.js
@@ -0,0 +1,52 @@
+(function (win) {
+  var Voluble = win.Voluble = win.Voluble || {};
+  var Backbone = win.Backbone, templates = Voluble.Templater;
+  templates.preCache('eventCreateInvite');  
+  templates.preCache('eventCreateDetails');  
+  templates.preCache('eventCreateSend');  
+  templates.preCache('eventCreateHeader');  
+  var keeper = {
+    'details': {
+      'id':'one',
+      'template':'eventCreateDetails'
+    },
+    'invite':{
+      'id':'two',
+      'template': 'eventCreateInvite'
+    },
+    'send':{
+      'id':'three',
+      'template': 'eventCreateSend'
+    }
+  };
+  Voluble.EventCreationView = Backbone.View.extend({
+    'id': 'eventCreation',
+    'el': $('.center_mid'),
+    'initialize': function (eventName) {
+      /*this.chats.bind('reset', this.render, this);*/
+      /*this.chats.bind('add', this.addChat, this);*/
+      this.render(eventName);
+    },
+    'render': function (eventName) {
+      var list = this;
+      var renderObj = {
+        'freshEvent': {
+          'id': eventName.id,
+          'fullName': eventName.id
+        }
+      };
+      templates.render('eventCreateHeader',function(template){
+        $(list.el).html(template(renderObj));
+        templates.render(keeper[eventName.action].template, function (template) {
+          $('#main_send').html(template(renderObj));
+          $('#'+keeper[eventName.action].id).addClass('active');
+        });
+      });
+      return this;
+    },
+    'close': function () {
+      $(this.el).unbind();
+      $(this.el).empty();
+    }
+  });
+})(this);
